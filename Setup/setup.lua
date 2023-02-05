1fdaf0db8ef8e0213bfad57d5032960b458aae84
Corbinhol <84656556+Corbinhol@users.noreply.github.com>
2023-02-05 12:32:28 -0600

    Update setup.lua

--- Setup/setup.lua
+++ Setup/setup.lua
@@ -1,2 +1,2 @@
-print("TESTING");
-print("TESTING 1 2 3");
+print("Beginning Setup");
+shell.run("wget https://raw.githubusercontent.com/Corbinhol/SevTech-OS/test/Setup/custom-basalt-installer.lua");
